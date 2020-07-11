31fa47300d65a9430b9abe2d423e613d42525e59
Umberto Valente <uvalente89@gmail.com>
2020-07-11 01:33:07 +0100

    feat: add 500 error handler

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,8 +1,15 @@
-from werkzeug.exceptions import NotFound
+from werkzeug.exceptions import NotFound, InternalServerError
 from flask import render_template
 from app.errors import errors_bp
+from app import db
+
 
 @errors_bp.app_errorhandler(NotFound)
-def not_found_error(error):
-    print('#############################')
+def page_not_found(error):
     return render_template('errors/404.html'), 404
+
+
+@errors_bp.app_errorhandler(InternalServerError)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('errors/500.html'), 500
