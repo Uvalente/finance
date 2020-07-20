fd0a282c2357032e78e537efe6daaa86ccf44406
Umberto Valente <uvalente89@gmail.com>
2020-07-20 16:45:51 +0100

    chore: update BuyForm shares field to number input

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, FloatField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, NumberRange
+from wtforms.widgets.html5 import NumberInput
 
 
 class QuoteForm(FlaskForm):
@@ -10,5 +11,6 @@ class QuoteForm(FlaskForm):
 
 class BuyForm(FlaskForm):
     symbol = StringField('Symbol', [DataRequired()])
-    shares = IntegerField('Shares', [DataRequired(), NumberRange(min=1)])
+    shares = IntegerField('Shares', [DataRequired(), NumberRange(
+        min=1)], widget=NumberInput(min=1))
     submit = SubmitField('Buy')
